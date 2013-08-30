b15e6f7bb43195001688d94bd572b281457d1472
luisco <luisco100@gmail.com>
2013-08-29 19:07:49 -0500

    calcula diferencia entre el directorio comprometido y el directorio original en su salida a producción

--- awebpwn.py
+++ awebpwn.py
@@ -5,7 +5,7 @@ import optparse
 import sqlite3
 import hashlib
 
-def scan_dir(root_dir):
+def scan_dir(root_dir, is_hash, table_files):
 	
 	fileList = []
 	fileSize = 0
@@ -13,54 +13,85 @@ def scan_dir(root_dir):
 	rootdir = root_dir
 	connection = sqlite3.connect('test.db')
 	cursor = connection.cursor()
-
-	cursor.execute('DELETE FROM files;')
+	table = "files" if (table_files == 0) else "files_original" 
+	name_file = ''
+	cursor.execute('DELETE FROM ' + table + ';')
 	
 	for root, subFolders, files in os.walk(rootdir):
 		folderCount += len(subFolders)
 		for myFile in files:
 			try:			
 				f = os.path.join(root,myFile)
+				name_file=f.replace(root_dir,'')
 				fileSize = fileSize + os.path.getsize(f)
 				fileDateCreate = os.path.getctime(f)
 				fileDateModified = os.path.getmtime(f)
 				fileSizeIndividual =  os.path.getsize(f)
 				
-				fileMD5 = hashlib.md5(open(f).read()).hexdigest() 
-				
-				file_vector = (f,fileDateCreate, fileDateModified,fileSizeIndividual,fileMD5,)
-				cursor.execute('INSERT INTO files (name_file, create_date, modified_date, size_file, md5) VALUES (?,?,?,?,?)', file_vector)
+				fileMD5 =  hashlib.md5(open(f).read()).hexdigest() if is_hash else '' 
+				file_vector = (name_file,fileDateCreate, fileDateModified,fileSizeIndividual,fileMD5,)
+				cursor.execute('INSERT INTO ' + table + ' (name_file, create_date, modified_date, size_file, md5) VALUES (?,?,?,?,?)', file_vector)
 				#print(f)
 				fileList.append(f)
-			except Exception, e:
-				print '[-] Archivo Sospechoso: ' + f + ' - ' + `e`
+			except Exception as inst:
+				cursor.execute('INSERT INTO ' + table + ' (name_file, value, comment) VALUES (?,?,?)', (name_file,1,"Posibles problemas de permisos al leer archivo"))
+				#print '[-] Archivo Sospechoso: ' + f + ' - ' + str(inst)
+
 
 	connection.commit()
 
-	print("[+] Total Size is {0} bytes".format(fileSize))
-	print("[+] Total Files ", len(fileList))
-	print("[+] Total Folders ", folderCount)
+	if(table_files == 0):
+		print("[+] Total Size is {0} bytes".format(fileSize))
+		print("[+] Total Files ", len(fileList))
+		print("[+] Total Folders ", folderCount)
 
-	cursor.execute('SELECT name_file, modified_date FROM files ORDER BY modified_date DESC LIMIT 0 , 5')
+		cursor.execute('SELECT name_file, modified_date FROM files ORDER BY modified_date DESC LIMIT 0 , 5')
+		rows = cursor.fetchall()
+
+		print("\n[+] Ultimos archivos modificados\n")
+		for row in rows:
+			print "%s %s" % (row[0], time.ctime(row[1]))
+
+def dir_dif():
+	connection = sqlite3.connect('test.db')
+	cursor = connection.cursor()
+	cursor.execute('SELECT name_file FROM files_original WHERE name_file NOT IN (SELECT name_file FROM files)')
 	rows = cursor.fetchall()
+	
+	print("\n[+] Archivos borrados en el directorio comprometido\n")
 
-	print("\n[+] Ultimos archivos modificados\n")
 	for row in rows:
-		print "%s %s" % (row[0], time.ctime(row[1]))
+		print "Archivo: " + row[0]
 
-
+	cursor.execute('SELECT name_file FROM files WHERE name_file NOT IN (SELECT name_file FROM files_original)')
+	rows2 = cursor.fetchall()
+	
+	print("\n[+] Archivos nuevos todos identificados como sospechosos\n")
+	
+	for row2 in rows2:
+		print "Archivo: " + row2[0]
 	
 def main():
 	
-	parser = optparse.OptionParser("usage %prog "+ "-d <root_dir>")
-	parser.add_option('-d', dest='rootDir', type='string', help='specify target host')
+	parser = optparse.OptionParser("usage %prog [options] <root_dir>", version="%prog 1.0")
+	parser.add_option('-m', '--md5', action="store_true", dest='is_hash', default= False, help='Generar Hash de los archivos para almecenar en la base de datos - Mas lento')
+	parser.add_option('-o', '--original', type='string', dest='dir_original', help='Directorio original el cual se comparara con el directorio comprometido')
 	(options , args) = parser.parse_args()
-	rootDir = options.rootDir
-	if(rootDir == None):
-		print "[-] Usted debe especificar el directorio raiz."
-		exit(0)
-	scan_dir(rootDir)
+    
+	# Error on invalid number of arguements
+	if len(args) < 1:
+        	parser.print_help()
+        	print ""
+	        sys.exit()
 
+	if os.path.exists(args[0]) == False:
+		parser.error("Directorio Invalido")
+	else:
+		scan_dir(args[0], options.is_hash, 0)
 
+	if options.dir_original:
+		if os.path.exists(options.dir_original):
+			scan_dir(options.dir_original, options.is_hash, 1)
+			dir_dif()
 if __name__ == '__main__':
 	main()

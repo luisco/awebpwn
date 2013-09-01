98a570e0befc49f12287aa362887c70b60d42ec6
luisco <luisco100@gmail.com>
2013-08-31 20:33:33 -0500

    Calcula arhivos infectados con el parametro -v

--- awebpwn.py
+++ awebpwn.py
@@ -4,6 +4,7 @@ import sys
 import optparse
 import sqlite3
 import hashlib
+import re
 
 def scan_dir(root_dir, is_hash, table_files):
 	
@@ -71,13 +72,62 @@ def dir_dif():
 	for row2 in rows2:
 		print "Archivo: " + row2[0]
 	
+
+def calcular_codigo_malicioso(root_dir):
+	connection = sqlite3.connect('test.db')
+        cursor = connection.cursor()
+	cursor.execute('SELECT name_file FROM files WHERE name_file like "%.php"')
+        rows = cursor.fetchall()
+	files = []
+        #print("\n[+] Listado de archivos con extension valida para el escaneo\n")
+	for row in rows:
+                files.append(root_dir + row[0])
+	
+	for data, filename, path_file in search_file_path(files):
+		if data:
+			valor_calculado = calcular(data, filename)
+			if valor_calculado > 0 :
+				my_filename = path_file + "/" + filename
+				my_filename = my_filename.replace(root_dir, '')
+				cursor.execute('UPDATE files SET value = '+ str(valor_calculado) + ' WHERE name_file = "' + str(my_filename) + '"') 	
+
+	cursor.execute('SELECT name_file FROM files WHERE value > 0 ORDER BY value DESC')
+        rows2 = cursor.fetchall()
+
+        print("\n[+] Archivos Identificados como sospechosos\n")
+
+        for row2 in rows2:
+                print "Archivo: " + row2[0]
+	
+
+def search_file_path(my_files):
+	for file in my_files:
+		filename = os.path.basename(file)
+		path_file = os.path.dirname(file)
+		try:
+			data = open(file).read()
+                except:
+                	data = False
+                	print "No puedo leer el archivo :: %s" % ( file )
+                yield data, filename, path_file	
+
+def calcular(data, filename):
+	if not data:
+		return "", 0
+		# Lots taken from the wonderful post at http://stackoverflow.com/questions/3115559/exploitable-php-functions
+	valid_regex = re.compile('(eval\(|file_put_contents|base64_decode|python_eval|exec\(|passthru|popen|proc_open|pcntl|assert\(|system\(|shell)', re.I)
+	matches = re.findall(valid_regex, data)
+	return len(matches)
+
 def main():
 	
 	parser = optparse.OptionParser("usage %prog [options] <root_dir>", version="%prog 1.0")
 	parser.add_option('-m', '--md5', action="store_true", dest='is_hash', default= False, help='Generar Hash de los archivos para almecenar en la base de datos - Mas lento')
 	parser.add_option('-o', '--original', type='string', dest='dir_original', help='Directorio original el cual se comparara con el directorio comprometido')
-	(options , args) = parser.parse_args()
-    
+	parser.add_option('-v', '--virus', action="store_true", dest='is_virus', default= False, help='Calcular si los archivos tienen codigo malicioso')
+        (options , args) = parser.parse_args()
+	
+
 	# Error on invalid number of arguements
 	if len(args) < 1:
         	parser.print_help()
@@ -93,5 +143,10 @@ def main():
 		if os.path.exists(options.dir_original):
 			scan_dir(options.dir_original, options.is_hash, 1)
 			dir_dif()
+
+	if options.is_virus:
+		if os.path.exists(args[0]):
+			calcular_codigo_malicioso(args[0])
+
 if __name__ == '__main__':
 	main()
